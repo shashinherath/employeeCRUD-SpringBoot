50b09e25f74c1999a8e939ef0af7c3fc4195f699
shashinherath <shashin.malinda@gmail.com>
2024-10-18 19:49:55 +0530

    Update UserRepository.java

--- src/main/java/com/employees/employeecrudspringboot/repository/UserRepository.java
+++ src/main/java/com/employees/employeecrudspringboot/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.employees.employeecrudspringboot.repository;
 
 import com.employees.employeecrudspringboot.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
