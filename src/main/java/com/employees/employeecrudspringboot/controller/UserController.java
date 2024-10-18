c078af22f896f7a02226d0d417623864bb230dfc
shashinherath <shashin.malinda@gmail.com>
2024-10-18 19:50:00 +0530

    Update UserController.java

--- src/main/java/com/employees/employeecrudspringboot/controller/UserController.java
+++ src/main/java/com/employees/employeecrudspringboot/controller/UserController.java
@@ -3,19 +3,22 @@ package com.employees.employeecrudspringboot.controller;
 import com.employees.employeecrudspringboot.dto.UserDTO;
 import com.employees.employeecrudspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api")
+@RequestMapping(value = "api/v1")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @GetMapping("/getuser")
     public List<UserDTO> getUser() {
-        return userService.getUsers();
+        return userService.getAllUsers();
     }
 }
